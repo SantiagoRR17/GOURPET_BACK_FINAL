185cef3394ca31f6fa0fd09a225ca7d89f694f4e
SantiagoRR17 <santiagorochar.03@gmail.com>
2025-11-26 20:43:29 -0500

    se eliminan los archivos de animal que no se van a utilizar

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-const parser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 3000;
-const animalRoutes = require("./routes/animal");
+const calificacionRoutes = require("./routes/calificaciones");
+const favoritoRoutes = require("./routes/favoritos");
+const recetaRoutes = require("./routes/recetas");
 const authRoutes = require("./routes/authentication");
 const mongoose = require("mongoose");
 require("dotenv").config();
-app.use(parser.urlencoded({ extended: false })); //permite leer los datos que vienen en la petición
-app.use(parser.json()); // transforma los datos a formato JSON //Gestión de las rutas usando el middleware
-app.use("/api", animalRoutes);
+app.use(express.urlencoded({ extended: false })); //permite leer los datos que vienen en la petición
+app.use(express.json()); // transforma los datos a formato JSON //Gestión de las rutas usando el middleware
 app.use("/api", authRoutes);
+app.use("/api", calificacionRoutes);
+app.use("/api", favoritoRoutes);
+app.use("/api", recetaRoutes);
 app.use(express.json()); //Conexión a la base de datos
 mongoose
   .connect(process.env.MONGODB_URI)
